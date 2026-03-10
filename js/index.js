5c4049bb035be53dd7b613f1fe17e84b3ec9c977
Hanna <206832485+klisterfisken@users.noreply.github.com>
2026-03-10 12:59:27 +0100

    Initial code for img animation

---
+++ js/index.js
@@ -0,0 +1,19 @@
+var initialSrc = "../images/dh-1.jpg";
+var scroll2Src = "../images/dh-2.jpg";
+var scroll3Src = "../images/dh-3.jpg";
+var scroll4Src = "../images/dh-4.jpg";
+var scroll5Src = "../images/dh-5.jpg";
+window.onscroll = function () { scrollAnimation() };
+function scrollAnimation() {
+    if (document.body.scrollTop > 212 || document.documentElement.scrollTop > 212) {
+        document.getElementById("dh-img").src = scroll5Src;
+    } else if (document.body.scrollTop > 159 || document.documentElement.scrollTop > 159) {
+        document.getElementById("dh-img").src = scroll4Src;
+    } else if (document.body.scrollTop > 106 || document.documentElement.scrollTop > 106) {
+        document.getElementById("dh-img").src = scroll3Src;
+    } else if (document.body.scrollTop > 53 || document.documentElement.scrollTop > 53) {
+        document.getElementById("dh-img").src = scroll2Src;
+    } else {
+        document.getElementById("dh-img").src = initialSrc;
+    }
+}
