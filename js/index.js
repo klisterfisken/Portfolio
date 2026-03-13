d4f8d0cea24d16b7067afb30c0eac67182bce4a6
Hanna <206832485+klisterfisken@users.noreply.github.com>
2026-03-13 18:39:43 +0100

    Add header scroll animation

--- js/index.js
+++ js/index.js
@@ -1,19 +1,18 @@
-// var initialSrc = "../images/dh-1.jpg";
-// var scroll2Src = "../images/dh-2.jpg";
-// var scroll3Src = "../images/dh-3.jpg";
-// var scroll4Src = "../images/dh-4.jpg";
-// var scroll5Src = "../images/dh-5.jpg";
-// window.onscroll = function () { scrollAnimation() };
-// function scrollAnimation() {
-//     if (document.body.scrollTop > 212 || document.documentElement.scrollTop > 212) {
-//         document.getElementById("dh-img").src = scroll5Src;
-//     } else if (document.body.scrollTop > 159 || document.documentElement.scrollTop > 159) {
-//         document.getElementById("dh-img").src = scroll4Src;
-//     } else if (document.body.scrollTop > 106 || document.documentElement.scrollTop > 106) {
-//         document.getElementById("dh-img").src = scroll3Src;
-//     } else if (document.body.scrollTop > 53 || document.documentElement.scrollTop > 53) {
-//         document.getElementById("dh-img").src = scroll2Src;
-//     } else {
-//         document.getElementById("dh-img").src = initialSrc;
-//     }
-// }
+const frames = document.querySelectorAll('.anim-frame');
+const scrollSpeed = 50;
+
+window.addEventListener('scroll', () => {
+    const scrollY = window.scrollY;
+
+    let index = Math.floor(scrollY / scrollSpeed);
+
+    if (index > frames.length - 1) index = frames.length - 1;
+
+    frames.forEach((frame, i) => {
+        if (i === index) {
+            frame.classList.add('active');
+        } else {
+            frame.classList.remove('active');
+        }
+    });
+});
