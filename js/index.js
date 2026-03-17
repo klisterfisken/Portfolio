9ac795fff86b5341b5bbf6818cdfd7280e795697
Hanna <206832485+klisterfisken@users.noreply.github.com>
2026-03-17 17:16:02 +0100

    Setup weather api call function

--- js/index.js
+++ js/index.js
@@ -16,3 +16,29 @@ window.addEventListener('scroll', () => {
         }
     });
 });
+
+
+const apiKey = "d43f2a110ed12f1279ce9d2dc56a6bd9";
+const city = "Uppsala";
+
+async function getWeatherData(city) {
+    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
+
+    const response = await fetch(apiUrl);
+
+    console.log(response);
+
+    return await response.json();
+}
+
+function displayWeaterInfo(data) {
+
+}
+
+function getWeaterEmoji(weatherId) {
+
+}
+
+// function displayError(message){
+
+// }
